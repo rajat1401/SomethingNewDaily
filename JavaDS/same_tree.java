3955027864574cbe4785113f08fb4bd891a6a76e
rajat_1401 <rajat16260@iiitd.ac.in>
2021-09-07 11:10:13 +0530

    Day8, same trees or not

---
+++ JavaDS/same_tree.java
@@ -0,0 +1,30 @@
+//problem link: https://leetcode.com/problems/same-tree/submissions/
+//problem statement: given roots of 2 trees, determine if they are same
+
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+
+class Solution {
+    public boolean isSameTree(TreeNode p, TreeNode q) {
+        if(p== null && q== null){
+            return true;
+        }
+        if(p!= null && q== null || q!=null && p== null){
+            return false;
+        }
+        return (p.val== q.val && isSameTree(p.left, q.left) && isSameTree(p.right,q.right));
+    }
+}
