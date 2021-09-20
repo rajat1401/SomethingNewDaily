ddb53bd04417983539678ed4b41bd116a6e8d874
rajat_1401 <rajat16260@iiitd.ac.in>
2021-09-20 11:45:42 +0530

    Day21, has repeated substring?

---
+++ PythonDS/has_repeated_substring.py
@@ -0,0 +1,18 @@
+#problem link: https://leetcode.com/problems/repeated-substring-pattern/submissions/
+#problem statement: check if string has a repeated substring or not
+
+class Solution:
+    def repeatedSubstringPattern(self, s: str) -> bool:
+        cur= 1
+        n= len(s)
+        while(cur<= n//2):
+            if(n%cur== 0):
+                flag= True
+                pattern= s[:cur]
+                for i in range(cur,n,cur):
+                    if(s[i:i+cur]!= pattern):
+                        flag= False
+                        break
+                if(flag): return flag
+            cur+= 1
+        return False
